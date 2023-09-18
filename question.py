e06e89db3fcdc8f98acc2767c541014671d94945
Rakshit Kaushik <kartikkaushik544@gmail.com>
2023-09-18 23:04:42 +0530

    Changes

--- question.py
+++ question.py
@@ -5,23 +5,20 @@ import sys
 pygame.init()
 
 # Constants
-WIDTH, HEIGHT = 600, 400
+WIDTH, HEIGHT = 700, 500  # Increased WIDTH to accommodate all options
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 FONT = pygame.font.Font(None, 36)
 QUESTION = "What is your favorite programming language?"
-OPTIONS = ["Python", "Java", "JavaScript", "C++"]
+OPTIONS = ["A. Python", "B. Java", "C. JavaScript", "D. C++"]
 
 # Create a Pygame window
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Question Pop-up")
 
 # Function to display the question and options
-
-
 def show_question():
     selected_option = None
-
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -31,17 +28,17 @@ def show_question():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 # Check if an option button was clicked
                 for i, option_text in enumerate(OPTIONS):
-                    option_rect = pygame.Rect(
-                        (WIDTH - 200) // 2,
+                    button_rect = pygame.Rect(
+                        (WIDTH - 400) // 2,
                         200 + i * 70,
-                        200,
+                        400,
                         50
                     )
-                    if option_rect.collidepoint(event.pos):
-                        selected_option = option_text
+                    if button_rect.collidepoint(event.pos):
+                        selected_option = option_text.split(". ")[1]
 
         screen.fill(WHITE)
-
+        
         # Draw the question
         question_text = FONT.render(QUESTION, True, BLACK)
         question_rect = question_text.get_rect(center=(WIDTH // 2, 100))
@@ -50,14 +47,13 @@ def show_question():
         # Create and draw option buttons
         for i, option in enumerate(OPTIONS):
             button_rect = pygame.Rect(
-                (WIDTH - 200) // 2,
+                (WIDTH - 400) // 2,
                 200 + i * 70,
-                200,
+                400,
                 50
             )
             pygame.draw.rect(screen, BLACK, button_rect)
-            pygame.draw.rect(screen, WHITE, button_rect.inflate(-10, -10))
-            option_text = FONT.render(option, True, BLACK)
+            option_text = FONT.render(option, True, WHITE)
             option_rect = option_text.get_rect(center=button_rect.center)
             screen.blit(option_text, option_rect)
 
@@ -65,11 +61,8 @@ def show_question():
 
         # If an option is selected, exit the loop and close the window
         if selected_option:
-            print(selected_option)
-            break
+            return selected_option
 
     pygame.quit()
 
-
-# Show the question pop-up
 # show_question()
